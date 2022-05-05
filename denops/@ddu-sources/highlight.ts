8a6183dcf6c6c27c128d17124e1f0e5932f64dab
matsui54 <matsui54@users.noreply.github.com>
2022-05-05 01:24:48 +0000

    :package: Update Deno dependencies
    Update dependencies by udd:
    
    /home/runner/work/ddu-source-highlight/ddu-source-highlight/denops/@ddu-kinds/highlight.ts
    [1/1] Looking for releases: https://deno.land/x/ddu_vim@v0.12.2/types.ts#^
    [1/1] Using latest: https://deno.land/x/ddu_vim@v0.12.2/types.ts#^
    
    /home/runner/work/ddu-source-highlight/ddu-source-highlight/denops/@ddu-sources/highlight.ts
    [1/2] Looking for releases: https://deno.land/x/ddu_vim@v1.4.0/types.ts
    [1/2] Attempting update: https://deno.land/x/ddu_vim@v1.4.0/types.ts -> v1.6.0
    [1/2] Update successful: https://deno.land/x/ddu_vim@v1.4.0/types.ts -> v1.6.0
    [2/2] Looking for releases: https://deno.land/x/ddu_vim@v1.4.0/deps.ts
    [2/2] Attempting update: https://deno.land/x/ddu_vim@v1.4.0/deps.ts -> v1.6.0
    [2/2] Update successful: https://deno.land/x/ddu_vim@v1.4.0/deps.ts -> v1.6.0
    
    /home/runner/work/ddu-source-highlight/ddu-source-highlight/README.md
    
    Already latest version:
    https://deno.land/x/ddu_vim@v0.12.2/types.ts#^ == v0.12.2
    
    Successfully updated:
    https://deno.land/x/ddu_vim@v1.4.0/types.ts v1.4.0 -> v1.6.0
    https://deno.land/x/ddu_vim@v1.4.0/deps.ts v1.4.0 -> v1.6.0
    make[1]: Entering directory '/home/runner/work/ddu-source-highlight/ddu-source-highlight'
    make[1]: Leaving directory '/home/runner/work/ddu-source-highlight/ddu-source-highlight'

--- denops/@ddu-sources/highlight.ts
+++ denops/@ddu-sources/highlight.ts
@@ -4,8 +4,8 @@ import {
   Actions,
   BaseSource,
   Item,
-} from "https://deno.land/x/ddu_vim@v1.4.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v1.4.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v1.6.0/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v1.6.0/deps.ts";
 
 type Params = Record<never, never>;
 type ActionData = { command: string };
